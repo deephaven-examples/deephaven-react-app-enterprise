b38a20e3560d249b5d821161288b31c6cc7aeee7
mikebender <mikebender@deephaven.io>
2025-01-08 12:07:06 -0500

    fix: Update example to use web package v0.102
    - Needed to wrap app with a ThemeProvider, since v0.52 added the Spectrum theming components: https://github.com/deephaven/web-client-ui/pull/1600
    - Tested against an Enterprise BHS and ensured the table appeared correctly

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "@deephaven/components/scss/BaseStyleSheet.scss";
 import App from "./App";
 import IFrameApp from "./IFrameApp";
+import { preloadTheme, ThemeProvider } from "@deephaven/components";
 
 const router = createBrowserRouter([
   {
@@ -18,9 +19,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+// Preload any cached theme variables to avoid a flash of unstyled content
+preloadTheme();
+
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider themes={null}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
